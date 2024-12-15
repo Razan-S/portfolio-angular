7438de5118fbb9b3a3376b86c102ba393cd45e08
schwynn <sschwynn@gmail.com>
2024-12-15 13:50:03 +0700

    feat: Implement initial structure for portfolio application with components and routing

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Angular18App
+# PortFolio Using Angular Framework
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.12.
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { WorksComponent } from './pages/works/works.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'works',
+        component: WorksComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent 
+    }
+];
